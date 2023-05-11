adef9c4c0e7f9c2f1009c498696977c190607bc3
Vel MAr <velmar.annabell@gmail.com>
2023-05-11 22:49:35 +0300

    add cube with persistance

---
+++ src/models/Cube.js
@@ -0,0 +1,22 @@
+const data = require('../data.json');
+const fs = require('fs');
+const path = require('path')
+
+class Cube {
+    constructor(name, description, imageUrl, difficultyLevel) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    static save(cube) {
+        console.log(data);
+        data.cubes.push(cube);
+        const jsonData = JSON.stringify(data,null,2);//to ad data in json file beauty arranged
+        console.log(jsonData);
+        fs.writeFileSync(path.resolve(__dirname, '../data.json'), jsonData)
+    }
+}
+
+module.exports = Cube
