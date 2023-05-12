7822ec24e6b83ea4b800a22778b69887c22d8b10
Vel MAr <velmar.annabell@gmail.com>
2023-05-12 15:03:43 +0300

    search works

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,29 @@
 const data = require('../data.json');
 
 exports.getHomepage = (req, res) => {
-    res.render('index', { cubes: data.cubes });
+    console.log(req.query);
+    const { search, from, to } = req.query;
+    let cubes = data.cubes;
+    console.log(cubes);
+    console.log('req.query', search);
+    if (search) {
+        cubes = cubes.filter(x => x.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+    };
+    if (from) {
+        cubes = cubes.filter(x => x.difficultyLevel >= from)
+    };
+    if (to) {
+        cubes = cubes.filter(x => x.difficultyLevel <= to)
+    };
+
+    res.render('index', { cubes: cubes, search ,from, to});
 };
 
 exports.getAboutPage = (req, res) => {
     res.render('about')
 };
 
-exports.getErrorPage=(req,res)=>{
+exports.getErrorPage = (req, res) => {
     res.render('404');
 };
 
